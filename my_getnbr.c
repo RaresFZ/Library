73cc7bf9e4bb6563ce2c1d0444d7786819c1228d
RaresFZ <146720100+RaresFZ@users.noreply.github.com>
2023-10-21 17:04:22 +0200

    T'abuse max

--- my_getnbr.c
+++ my_getnbr.c
@@ -17,15 +17,15 @@ int my_getnbr(char const *str)
     while (str[i] == '+' || str[i] == '-') {
         if (str[i] == '-') {
             sign = sign * -1;
-        i++;
         }
+        i++;
     }
     while (str[i] != '\0') {
         if (str[i] >= '0' && str[i] <= '9') {
             nb = nb * 10;
             nb = nb + str[i] - '0';
             i++;
-                } else {
+        } else {
             return (nb * sign);
         }
     }
