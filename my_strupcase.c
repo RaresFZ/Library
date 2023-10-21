07263869cea37dd5f85ce557848eca43b2b43f47
RaresFZ <146720100+RaresFZ@users.noreply.github.com>
2023-10-21 17:57:05 +0200

    TON MAIN DE MERDE

--- my_strupcase.c
+++ my_strupcase.c
@@ -11,13 +11,13 @@
 
 char *my_strupcase(char *str)
 {
-    int len = my_strlen(&str);
-    int i;
+    int i = 1;
 
-    for (i = 0; i < len; i++) {
+    while (str[i] != '\0') {
         if (str[i] >= 97 && str[i] <= 122) {
             str[i] = str[i] - 32;
         }
-        return 0;
+        i++;
     }
+    return str;
 }
