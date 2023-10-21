2be0da7df68825e864bfae8b7093f601b279bc11
Max-Epinat <146720699+Max-Epinat@users.noreply.github.com>
2023-10-21 16:58:07 +0200

    Add files via upload

--- my_getnbr.c
+++ my_getnbr.c
@@ -2,10 +2,32 @@
 ** EPITECH PROJECT, 2023
 ** my_getnbr.c
 ** File description:
-** m
+** a function that returns a number,
+** sent to the function as a string.
 */
+
+#include <stdio.h>
 
 int my_getnbr(char const *str)
 {
-    return 0;
+    int nb = 0;
+    int sign = 1;
+    int i = 0;
+
+    while (str[i] == '+' || str[i] == '-') {
+        if (str[i] == '-') {
+            sign = sign * -1;
+        i++;
+        }
+    }
+    while (str[i] != '\0') {
+        if (str[i] >= '0' && str[i] <= '9') {
+            nb = nb * 10;
+            nb = nb + str[i] - '0';
+            i++;
+                } else {
+            return (nb * sign);
+        }
+    }
+    return (nb * sign);
 }
