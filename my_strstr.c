dbd32c95c1859cced4eb54560abd3c5d518ce49f
Max-Epinat <146720699+Max-Epinat@users.noreply.github.com>
2023-10-22 15:57:26 +0200

    Add files via upload

--- my_strstr.c
+++ my_strstr.c
@@ -2,10 +2,26 @@
 ** EPITECH PROJECT, 2023
 ** my_strstr.c
 ** File description:
-** m
+** A function that search the first occurence of
+** the second string in the first one.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
 
 char *my_strstr(char *str, char const *to_find)
 {
-    return 0;
+    int	i = 0;
+
+    if (str[0] != '\0') {
+        while (to_find[i] != '\0') {
+            if (to_find[i] != str[i]) {
+                return (my_strstr(str + 1, to_find));
+            }
+            i++;
+        }
+        return str;
+    } else {
+        return 0;
+    }
 }
