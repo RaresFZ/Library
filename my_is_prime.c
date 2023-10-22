3dffe855c99d7cde752d88a9361b5198c27549eb
RaresFZ <146720100+RaresFZ@users.noreply.github.com>
2023-10-22 15:49:12 +0200

    LET S GO

--- my_is_prime.c
+++ my_is_prime.c
@@ -7,5 +7,17 @@
 
 int my_is_prime(int nb)
 {
+    int i;
+
+    i = 2;
+    if (nb < 2) {
+        return 1;
+    }
+    while (i < nb) {
+        if (nb % i == 0) {
+            return 1;
+        }
+        i++;
+    }
     return 0;
 }
